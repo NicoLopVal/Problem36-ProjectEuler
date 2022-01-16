e153b2f1d6ebaab9e81def98aad3da4049a251b0
nikil <nikil@DESKTOP-73MHGNO>
2022-01-16 10:14:31 -0500

    Add project files.

---
+++ Problem36-ProjectEuler/Program.cs
@@ -0,0 +1,35 @@
+﻿int binBase = 2;
+
+double totalSum = 0;
+
+long thisInt = BitConverter.DoubleToInt64Bits(3);
+
+for (int i = 0; i< 1000000; i++)
+{
+    string binNumber = Convert.ToString(i, binBase);
+    if (isPalindrome(i.ToString()) && isPalindrome(binNumber))
+    {
+        totalSum = totalSum + i;
+    }
+}
+
+Console.WriteLine("The sum of all numbers, less than one million, which are palindromic in base 10 and base 2 is: " + totalSum);
+
+bool isPalindrome(string strValue)
+{
+    bool isPalindrome = true;
+    int j = strValue.Length - 1;
+    for(int i = 0; i< strValue.Length; i++)
+    {
+        if (strValue[i] == strValue[j])
+            j--;
+        else
+        {
+            isPalindrome = false;
+            break;
+        }
+        if (j <= i)
+            break;
+    }
+    return isPalindrome;
+}
